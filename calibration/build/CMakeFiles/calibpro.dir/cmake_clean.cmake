e0a92809982bb0294b7b14be611732a31548b746
westwell <xinfeng.shen@westwell-lab.com>
2020-04-27 14:31:56 +0800

    change code structure

---
+++ calibration/build/CMakeFiles/calibpro.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibpro.dir/camera_calibration.cpp.o"
+  "calibpro.pdb"
+  "calibpro.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibpro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
